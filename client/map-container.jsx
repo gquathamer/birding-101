1261a2e1d89f275cfd3df1fdc98f350b5c8228fa
Garrett Quathamer <gquathamer@gmail.com>
2023-05-10 16:24:27 -0600

    successfully created a recent observation map showing submitted observations to ebird in the last 30 days for jefferson county co

--- client/map-container.jsx
+++ client/map-container.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 export default function Map() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const ebirdRecentObservationsURL = new URL(
+      'https://api.ebird.org/v2/data/obs/US-CO-059/recent',
+    );
+
+    fetch(ebirdRecentObservationsURL, {
+      headers: {
+        'x-ebirdapitoken': 'i6k4lfj8nhr9',
+      },
+    })
+      .then(response => response.json())
+      .then(response => {
+        setData(response);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[39.67988240776321, -105.07969112565975]}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[39.67988240776321, -105.07969112565975]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {data.map((obs, idx) => {
+        return (
+          <Marker key={idx} position={[obs.lat, obs.lng]}>
+            <Popup>
+              {`${obs.howMany} ${obs.comName} spotted at ${obs.locName}`}
+            </Popup>
+          </Marker>
+        );
+      })}
     </MapContainer>
   );
 }
